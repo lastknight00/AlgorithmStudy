88f5f6228ccce6e051b1588f970daca08a2848f5
taehan Lee <darkknight8608@gmail.com>
2017-08-03 23:42:25 +0900

    Element 정보 정리.

--- js/elements.js
+++ js/elements.js
@@ -1,13 +1,13 @@
 var makeFunctionList = key => {
-  return $('<div>').html(key + '(Parameters : ' + algoMap[key].params
-    + ', Return Type : ' + algoMap[key].returnType + ')').attr('name', key).attr('draggable', true).addClass('func_list')
+  return $('<div>').html(key + '(Parameters : ' + algoMap[key].params + ', Return Type : ' + algoMap[key].returnType + ')')
+  .attr('draggable', true).addClass('func_list')
   .on('dragstart', e => {
-    e.originalEvent.dataTransfer.setData('key', $(e.target).attr('name'));
+    e.originalEvent.dataTransfer.setData('key', key);
   });
 };
 
 var makeInputElement = key => {
-  return $('<input>').addClass(key).on('drop', e => {
+  return $('<input>').on('drop', e => {
     if($(e.target).parent().children(':first-child').html() == e.originalEvent.dataTransfer.getData('returnType')) {
       alert('true');
     } else {
@@ -26,11 +26,11 @@ var makeFunctionDiv = funcName => {
   var funcDiv = $('<div>').addClass('func').append($('<h3>').html(funcName));
   funcDiv.append($('<p>').addClass('input').html('Input'));
   func.params.forEach(function(key) {
-    var element = $('<div>').attr('id', key);
+    var element = $('<div>');
     element.append($('<h4>').html(key));
     switch (key) {
       case 'list':
-        element.append($('<button>').attr('name', key).html('Add Element').on('click', function(e) {
+        element.append($('<button>').html('Add Element').on('click', function(e) {
           $(e.target).prev().append(makeInputElement(key));
         }));
         break;
@@ -42,7 +42,8 @@ var makeFunctionDiv = funcName => {
     }
     funcDiv.append(element);
   });
-  funcDiv.append($('<p>').addClass('output').html('Output').append($('<h4>').html(func.returnType).attr('draggable', true).on('dragstart', e => {
+  funcDiv.append($('<p>').addClass('output').html('Output').append($('<h4>').html(func.returnType)
+    .attr('draggable', true).on('dragstart', e => {
     e.originalEvent.dataTransfer.setData('returnType', $(e.target).html());
   })));
   return funcDiv;
